9174cbc92b0d06790f167f8221b65fa712edc4ec
Telegram & App source Code <khithlainhtet2021@gmail.com>
2026-02-26 08:37:15 -0600

    PandaXMusic

--- PandaXMusic/plugins/sudo/sudoers.py
+++ PandaXMusic/plugins/sudo/sudoers.py
@@ -48,7 +48,7 @@ async def userdel(client, message: Message, _):
 @language
 async def sudoers_list(client, message: Message, _):
     if message.from_user.id not in SUDOERS:
-        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/HANTHAR999'>🇷🇺⛦°𝐏𝐀𝐍𝐃𝐀` 𓆩🇽𓆪 𝗞𝗜𝗡𝗚🇳</a>",
+        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/HANTHAR999'>⛦°𝐏𝐀𝐍𝐃𝐀 𓆩🇽𓆪 𝗞𝗜𝗡𝗚🇳</a>",
         disable_web_page_preview=True,
         parse_mode="html")
     text = _["sudo_5"]
